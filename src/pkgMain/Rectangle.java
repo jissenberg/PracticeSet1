507453bedca825759b536b6dd67727bb9b002826
jessissenberg <jessissenberg@10.0.0.189>
2016-09-06 09:30:01 -0400

    I changed the '0' to the area formula, which is length times width

--- src/pkgMain/Rectangle.java
+++ src/pkgMain/Rectangle.java
@@ -18,6 +18,7 @@ public class Rectangle {
 	public double Area()
 	{
 		// TODO Implement Area() function with the correct formula
-		return 0;
+		//I changed the '0' to the area formula, which is length times width 
+		return Length*Width;
 	}	
 }
